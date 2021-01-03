e3678356c53652ccc4703da53e19c9c89947668e
wyc192273 <wyczk@sina.com>
2021-01-03 16:47:10 +0800

    reformat

--- Week_09/rpcfx-provider/src/main/java/com/wyc/rpcfx/provider/RpcfxServerApplication.java
+++ Week_09/rpcfx-provider/src/main/java/com/wyc/rpcfx/provider/RpcfxServerApplication.java
@@ -1,7 +1,5 @@
 package com.wyc.rpcfx.provider;
 
-import java.lang.reflect.InvocationTargetException;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
